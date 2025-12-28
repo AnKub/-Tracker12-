9b59745f7ba862970b112bc3b053976c460eee4d
AnKub <ankub3088@gmail.com>
2025-12-28 11:21:56 +0100

    settings

--- src/store/useSettingsStore.ts
+++ src/store/useSettingsStore.ts
@@ -16,4 +16,14 @@ type SettingsStore = SettingsState & SettingsActions;
 export const useSettingsStore = create<SettingsStore>((set)=> ({
   currency: 'UAH',
   language: 'en',
+
+  setCurrency: (currency) => {set({currency});
+},
+
+setLanguage: (language) => {set({language});
+},
+
+loadSettings: () => {
+  const savedCurrency = localStorage.getItem('settings_currency') as SettingsState['currency'] | null;
+  const savedLanguage = localStorage.getItem('settings_language') as SettingsState['language'] | null;
 }))
