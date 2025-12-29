d286609d187f4e04ad8daebd0b6db0ae78341d64
AnKub <ankub3088@gmail.com>
2025-12-29 19:36:02 +0100

    settings

--- src/components/ui/Settings/Settings.tsx
+++ src/components/ui/Settings/Settings.tsx
@@ -1,51 +1,51 @@
-// import React from 'react';
-// import './Settings.scss';
-// import {useTheme} from '../../../hooks/useTheme';
-// import {useSettingsStore} from '../../../store/useSettingsStore';
+import React from 'react';
+import './Settings.scss';
+import {useTheme} from '../../../hooks/useTheme';
+import {useSettingsStore} from '../../../store/useSettingsStore';
 
-// interface  SettingsProps {
-//   isOpen: boolean;
-//   onClose: () => void;
-// }
+interface  SettingsProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
 
-// export const Settings: React.FC<SettingsProps> = ({isOpen, onClose}) => {
-//   const {theme, toggleTheme} = useTheme();
-//   const {currency, setCurrency} = useSettingsStore();
+export const Settings: React.FC<SettingsProps> = ({isOpen, onClose}) => {
+  const {theme, toggleTheme} = useTheme();
+  const {currency, setCurrency} = useSettingsStore();
 
-//   if (!isOpen) return null;
+  if (!isOpen) return null;
 
-//   return (
-//     <div className="settings">
-//       <div className="settings__content">
-//         <div className="settings__header">
-//           <h2>Settings</h2>
-//           <button onClick={onClose} className='settings__close'>x</button>
-//         </div>
-//         <div className="settings__body">
-//                   <div className="setting-item">
-//                     <span className="setting-item__label">Dark Theme</span>
-//                     <button onClick={toggleTheme}
-//                     className={`toggle-btn ${theme === 'dark'? 'toggle-btn--active': ''}`}
-//                     >
-//                         {theme === 'dark' ? '🌙' : '☀️'}
-//                     </button>
-//                   </div>
-//                   <div className="setting-item">
-//                     <span className="setting-item__label">Currency</span>
-//                     <select 
-//                       value={currency}
-//                       onChange={(e) => setCurrency(e.target.value as 'UAH' | 'USD' | 'EUR')}
-//                       className="setting-select"
-//                     >
-//                       <option value="UAH">🇺🇦 UAH (₴)</option>
-//                       <option value="USD">🇺🇸 USD ($)</option>
-//                       <option value="EUR">🇪🇺 EUR (€)</option>
-//                     </select>
-//           </div> 
-//         </div>
-//       </div>
-//     </div>
-//   ) ;
+  return (
+    <div className="settings">
+      <div className="settings__content">
+        <div className="settings__header">
+          <h2>Settings</h2>
+          <button onClick={onClose} className='settings__close'>x</button>
+        </div>
+        <div className="settings__body">
+                  <div className="setting-item">
+                    <span className="setting-item__label">Dark Theme</span>
+                    <button onClick={toggleTheme}
+                    className={`toggle-btn ${theme === 'dark'? 'toggle-btn--active': ''}`}
+                    >
+                        {theme === 'dark' ? '🌙' : '☀️'}
+                    </button>
+                  </div>
+                  <div className="setting-item">
+                    <span className="setting-item__label">Currency</span>
+                    <select 
+                      value={currency}
+                      onChange={(e) => setCurrency(e.target.value as 'UAH' | 'USD' | 'EUR')}
+                      className="setting-select"
+                    >
+                      <option value="UAH">🇺🇦 UAH (₴)</option>
+                      <option value="USD">🇺🇸 USD ($)</option>
+                      <option value="EUR">🇪🇺 EUR (€)</option>
+                    </select>
+          </div> 
+        </div>
+      </div>
+    </div>
+  ) ;
   
   
-// };
+};
