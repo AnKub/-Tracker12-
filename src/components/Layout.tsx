31431d792102e1cc73913ae2b9dd6cade574b778
AnKub <ankub3088@gmail.com>
2026-02-13 12:23:21 +0100

    part

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,25 +1,26 @@
-// import React from 'react';
-// import { Header } from './ui/Header';
-// import Game from './pages/TicTac/Game';
-// import { Routes, Route } from 'react-router-dom';
-// import './Layout.scss';
+import React from 'react';
+import { Header } from './ui/Header';
+import Game from './pages/TicTac/Game';
+import { Routes, Route } from 'react-router-dom';
+import './Layout.scss';
 
-// interface LayoutProps {
-//   children: React.ReactNode;
-// }
+interface LayoutProps {
+  children: React.ReactNode;
+}
 
-// export const Layout: React.FC<LayoutProps> = ({ children }) => {
-//   return (
-//     <div className="layout">
-//       <Header />
-//       <Routes>
-//         <Route path="/tic-tac" element={<Game />} />
-//       </Routes>
-//       <main className="layout__main">
-//         <div className="layout__content">
-//           {children}
-//         </div>
-//       </main>
-//     </div>
-//   );
-// };
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="layout">
+      <Header />
+      <Routes>
+        <Route path="/tic-tac" element={<Game />} />
+      </Routes>
+      <main className="layout__main">
+        <div className="layout__content">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+};
+
