4ad9953f902d638636c7c15a686f9dc2b94b7bb2
AnKub <ankub3088@gmail.com>
2026-02-18 15:20:36 +0100

    memoref

--- src/components/ui/study/Callback.tsx
+++ src/components/ui/study/Callback.tsx
@@ -1,41 +1,40 @@
-import React, {useEffect, useState} from 'react';
+// import React, {useEffect, useState} from 'react';
 
-function useLogger(value){
-  useEffect(() => {
-    console.log('Value changed: ', value)
-  },[value])
-}
+// function useLogger(value){
+//   useEffect(() => {
+//     console.log('Value changed: ', value)
+//   },[value])
+// }
 
-function useInput(initialValue){
-  const[value,setValue] = useState(initialValue)
+// function useInput(initialValue){
+//   const[value,setValue] = useState(initialValue)
 
-  const onChange = event => {
-    setValue(event.target.value)
-  }
+//   const onChange = event => {
+//     setValue(event.target.value)
+//   }
 
-  const clear = () => setValue('')
-  return {
-    bind:{value, onChange},
-    value,
-    clear
-  }
-}
+//   const clear = () => setValue('')
+//   return {
+//     bind:{value, onChange},
+//     value,
+//     clear
+//   }
+// }
 
 
-function App (){
-const input = useInput('') 
- useLogger(input.value)
+// function App (){
+// const input = useInput('') 
+//  useLogger(input.value)
 
-  return (
-    <div className="container pt-3">
+//   return (
+//     <div className="container pt-3">
       
-      <input type="text" {...input.bind} />
-      <button onClick={input.clear}>Clear</button>
-           <hr />
-       <h1>{input.value} </h1>
+//       <input type="text" {...input.bind} />
+//       <button onClick={input.clear}>Clear</button>
+//            <hr />
+//        <h1>{input.value} </h1>
        
-    </div>
-    HHH
+//     </div>
 
-  )
-}
+//   )
+// }
