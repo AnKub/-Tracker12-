df019c984e44a0e7ed1933cfe0ca1275fc9794fc
AnKub <ankub3088@gmail.com>
2026-02-24 10:16:47 +0100

    extrathings

---
+++ src/components/ui/TransactionSearch.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+type TransactionSearchProps = {
+  onSearch:(query: string) => void;
+};
+
+export const TransactionSearch: React.FC<TransactionSearchProps> = ({onSearch}) => {
+   const [query, setQuery] = useState('');
+
+   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    onSearch(e.target.value);
+   } 
+   return (
+    <div className="transaction-search">
+      <input type="text"
+      placeholder='Search transactions...'
+      value={query}
+      onChange={handleInputChange}
+      className="transaction-search__input"
+      />
+    </div>
+   )
+   
+};
