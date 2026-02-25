aba3e9e5ece18de241faa5560134d4b90703acea
AnKub <ankub3088@gmail.com>
2026-02-25 11:22:19 +0100

    extrathings

--- src/components/ui/TransactionSearch.tsx
+++ src/components/ui/TransactionSearch.tsx
@@ -1,26 +1,26 @@
-// import React, {useState} from 'react';
-// import './TransactionSearch.scss'
+import React, {useState} from 'react';
+import './TransactionSearch.scss'
 
-// type TransactionSearchProps = {
-//   onSearch:(query: string) => void;
-// };
+type TransactionSearchProps = {
+  onSearch:(query: string) => void;
+};
 
-// export const TransactionSearch: React.FC<TransactionSearchProps> = ({onSearch}) => {
-//    const [query, setQuery] = useState('');
+export const TransactionSearch: React.FC<TransactionSearchProps> = ({onSearch}) => {
+   const [query, setQuery] = useState('');
 
-//    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setQuery(e.target.value);
-//     onSearch(e.target.value);
-//    } 
-//    return (
-//     <div className="transaction-search">
-//       <input type="text"
-//       placeholder='Search transactions...'
-//       value={query}
-//       onChange={handleInputChange}
-//       className="transaction-search__input"
-//       />
-//     </div>
-//    )
+   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    onSearch(e.target.value);
+   } 
+   return (
+    <div className="transaction-search">
+      <input type="text"
+      placeholder='Search transactions...'
+      value={query}
+      onChange={handleInputChange}
+      className="transaction-search__input"
+      />
+    </div>
+   )
    
-// };
+};
